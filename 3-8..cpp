ed5aee805d18dfa02737681aa0a10db13e78ebf6
wataru <sadakata@gmail.com>
2022-04-15 17:08:50 +0900

    3-8 完全正解

---
+++ 3-8..cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+  int n;
+  cout << "何個*を表示しますか？\n";
+  cin >> n;
+
+  for (int i = 0; i < n; i++){
+    cout << "*";
+  }
+  cout << endl;
+}
